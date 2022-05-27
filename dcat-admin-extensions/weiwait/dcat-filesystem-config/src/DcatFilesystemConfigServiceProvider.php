89c512bbbc7da46672d240d5e9f37561fca1cb9f
weiwait <mail@weiwait.cn>
2022-05-27 17:37:41 +0800

    升级文件系统配置

--- dcat-admin-extensions/weiwait/dcat-filesystem-config/src/DcatFilesystemConfigServiceProvider.php
+++ dcat-admin-extensions/weiwait/dcat-filesystem-config/src/DcatFilesystemConfigServiceProvider.php
@@ -4,6 +4,7 @@ namespace Weiwait\Filesystem;
 
 use Dcat\Admin\Extend\ServiceProvider;
 use Dcat\Admin\Admin;
+use Illuminate\Support\Facades\Event;
 use Weiwait\Filesystem\Models\FilesystemConfig;
 
 class DcatFilesystemConfigServiceProvider extends ServiceProvider
@@ -24,41 +25,47 @@ class DcatFilesystemConfigServiceProvider extends ServiceProvider
 	{
 		parent::init();
 
-        config()->set('filesystems.disks.oss', [
-            'access_key' => FilesystemConfig::get('oss_access_key'),
-            'secret_key' => FilesystemConfig::get('oss_secret_key'),
-            'endpoint' => FilesystemConfig::get('oss_endpoint'),
-            'bucket' => FilesystemConfig::get('oss_bucket'),
-            'driver' => 'oss',
-            'root' => '',
-            'isCName' => false,
-        ]);
+        app()->booted(function () {
+            config()->set('filesystems.disks.oss', [
+                'access_key' => FilesystemConfig::get('oss_access_key'),
+                'secret_key' => FilesystemConfig::get('oss_secret_key'),
+                'endpoint' => FilesystemConfig::get('oss_endpoint'),
+                'bucket' => FilesystemConfig::get('oss_bucket'),
+                'driver' => 'oss',
+                'root' => '',
+                'isCName' => false,
+            ]);
 
-        config()->set('filesystems.disks.qiniu', [
-            'access_key' => FilesystemConfig::get('qiniu_access_key'),
-            'secret_key' => FilesystemConfig::get('qiniu_secret_key'),
-            'domain' => FilesystemConfig::get('qiniu_domain'),
-            'bucket' => FilesystemConfig::get('qiniu_bucket'),
-            'driver' => 'qiniu',
-        ]);
+            config()->set('filesystems.disks.qiniu', [
+                'access_key' => FilesystemConfig::get('qiniu_access_key'),
+                'secret_key' => FilesystemConfig::get('qiniu_secret_key'),
+                'domain' => FilesystemConfig::get('qiniu_domain'),
+                'bucket' => FilesystemConfig::get('qiniu_bucket'),
+                'driver' => 'qiniu',
+            ]);
 
-        config()->set('filesystems.disks.cos', [
-            'app_id' => FilesystemConfig::get('cos_app_id'),
-            'secret_id' => FilesystemConfig::get('cos_secret_id'),
-            'secret_key' => FilesystemConfig::get('cos_secret_key'),
-            'region' => FilesystemConfig::get('cos_region'),
-            'bucket' => FilesystemConfig::get('cos_bucket'),
-            'cnd' => FilesystemConfig::get('cos_cdn'),
-            'signed_url' => false,
-            'driver' => 'cos',
-            'prefix' => '',
-            'guzzle' => [
-                'timeout' => 60,
-                'connect_timeout' => 60,
-            ],
-        ]);
+            config()->set('filesystems.disks.cos', [
+                'app_id' => FilesystemConfig::get('cos_app_id'),
+                'secret_id' => FilesystemConfig::get('cos_secret_id'),
+                'secret_key' => FilesystemConfig::get('cos_secret_key'),
+                'region' => FilesystemConfig::get('cos_region'),
+                'bucket' => FilesystemConfig::get('cos_bucket'),
+                'cnd' => FilesystemConfig::get('cos_cdn'),
+                'signed_url' => false,
+                'driver' => 'cos',
+                'prefix' => '',
+                'guzzle' => [
+                    'timeout' => 60,
+                    'connect_timeout' => 60,
+                ],
+            ]);
 
-		config()->set('filesystems.default', FilesystemConfig::get('disk', 'public'));
+            config()->set('filesystems.default', FilesystemConfig::get('disk', config('filesystems.default')));
+        });
+
+        Event::listen('admin:booted', function () {
+            config()->set('admin.upload.disk', FilesystemConfig::get('disk', config('admin.upload.disk')));
+        });
 	}
 
 	public function settingForm()
--- dcat-admin-extensions/weiwait/dcat-filesystem-config/version.php
+++ dcat-admin-extensions/weiwait/dcat-filesystem-config/version.php
@@ -5,4 +5,13 @@ return [
         'Initialize extension.',
         'create_filesystem_configs_table.php'
     ],
+    '2.0.0' => [
+        '更新依赖到laravel9'
+    ],
+    '2.0.1' => [
+        '修复配置加载'
+    ],
+    '2.0.2' => [
+        '修复全局配置加载'
+    ],
 ];
